9a8b6b5623eb1273f014b0babbd43b353960fe49
platisd <platisd@users.noreply.github.com>
2020-06-08 15:20:18 +0200

    Change iteration numbers

--- src/feature.cpp
+++ src/feature.cpp
@@ -5,7 +5,7 @@ int main()
     std::cout << "The awesome magic number is: " << 5 + 5;
     std::cout << "There are many magic numbers!" << std::endl;
 
-    for (int x = 0; x < 10; i++)
+    for (int x = 0; x < 200; i++)
     {
         std::cout << i << std::endl;
     }
